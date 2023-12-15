d5152646164ab144cb52de176a3cd242eea50c5c
Nguyễn Hữu Đức Vũ <ducvuglotec@gmail.com>
2023-12-15 12:51:15 +0700

    add table

---
+++ server/database/migrations/2023_12_15_054608_create_post_detail_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_detail', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('desc');
+            $table->text('content');
+            $table->string('avatar_post');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_detail');
+    }
+};
