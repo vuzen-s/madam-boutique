a9f94b8bd47b5e7914e418641d1e6ed426db2db2
sontran-q7 <anhsontran.q7@gmail.com>
2024-01-04 01:23:14 +0700

    1

--- client/src/components/BlogList/BlogList.jsx
+++ client/src/components/BlogList/BlogList.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect } from "react";
-import BlogItem from "./BlogItem";
-import DataBlog from "./DataBlog";
-import { useState } from "react";
+// import React, { useEffect } from "react";
+// import BlogItem from "./BlogItem";
+// import DataBlog from "./DataBlog";
+// import { useState } from "react";
 
-const BlogList = () => {
-  const [designer, setDesigner] = useState([]);
+// const BlogList = () => {
+//   const [designer, setDesigner] = useState([]);
 
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/design')
-      .then((response) => response.json())
-      .then((data) => {
-        setDesigner(data.designers);
-        console.log(data.designers)
-      })
-      .catch((err) => {
-        setDesigner(null);
-      })
-  }, [])
+//   useEffect(() => {
+//     fetch('http://127.0.0.1:8000/api/design')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setDesigner(data.designers);
+//         console.log(data.designers)
+//       })
+//       .catch((err) => {
+//         setDesigner(null);
+//       })
+//   }, [])
 
-  return (
-    <div className="blog-list">
-      {DataBlog.map((blog) => (
-        <BlogItem
-          key={blog.id}
-          title={blog.title}
-          desc={blog.desc}
-          imageUrl={blog.imageUrl}
-        />
-      ))}
+//   return (
+//     <div className="blog-list">
+//       {DataBlog.map((blog) => (
+//         <BlogItem
+//           key={blog.id}
+//           title={blog.title}
+//           desc={blog.desc}
+//           imageUrl={blog.imageUrl}
+//         />
+//       ))}
    
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default BlogList;
+// export default BlogList;
